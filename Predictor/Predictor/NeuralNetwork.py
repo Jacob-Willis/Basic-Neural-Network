d31bafa2def987fd08990d8ea1cdecb11d8bf8e6
Jacob_Willis <jacebly@gmail.com>
2019-06-24 23:23:27 +1000

    Test images added and self learning implemented.

--- Predictor/Predictor/NeuralNetwork.py
+++ Predictor/Predictor/NeuralNetwork.py
@@ -1,23 +1,205 @@
 # Imports
-import numpy as np 
+import numpy as np
 
-class NeuralNetwork: 
+class NeuralNetwork:
     # Initialisation method that sets up all the biases and weights and the
-    # shape weights before using them to make predictions
-    def __init__(self, layer_sizes): 
-        weight_shapes = [(a,b) for a,b in zip(layer_sizes[1:],layer_sizes[:-1])] 
+    # Shape weights before using them to make predictions
+    def __init__(self, layer_sizes):
+        weight_shapes = [(a, b) for a, b in zip(layer_sizes[1:], layer_sizes[:-1])] 
         self.weights = [np.random.standard_normal(s) / s[1] ** 0.5 for s in weight_shapes] 
-        self.biases = [np.zeros((s,1)) for s in layer_sizes[1:]] 
+        self.biases = [np.zeros((s, 1)) for s in layer_sizes[1:]] 
+
+        self.num_layers = len(layer_sizes)
+        self.activations = np.asarray([np.zeros(size) for size in layer_sizes])
     
-    # This method uses the biases and weights to come up with a prediction and
-    # returns the prediction
+    def feed_forward(self, sample):
+        # Feed a sample through the network, and return the prediction
+
+        # Input layer is first activation layer
+        self.activations[0] = sample
+
+        # Forward propogation
+        for i in range(self.num_layers - 1):
+            # z is the 'weighted input'
+            z = np.matmul(self.weights[i], self.activations[i]) + self.biases[i]
+            self.activations[i + 1] = self.activation_function(z)
+
+        # The output layer is the final activation layer
+        return self.activations[-1]
+
+    def train_sgd(self, training_images, training_labels, epochs, batch_size, learning_rate):
+        # Trains using the Stochastic Gradient Decent
+        training_data = [(x, y) for x, y in zip(training_images, training_labels)]
+
+        print('Start training:')
+        for j in range(epochs):
+            batches = [training_data[k:k + batch_size] for k in range(0, len(training_data), batch_size)]
+
+            for batch in batches:
+                self.update_batch(batch, learning_rate)
+
+            print('Epoch {0} complete'.format(j))
+
+
+    def update_batch(self, batch, learning_rate):
+        """
+        Calculate direction of gradient given a batch, and then apply this change to weights and biases
+        These arrays hold the change in direction for each weight and bias
+        """
+        bias_gradients = [np.zeros(b.shape) for b in self.biases]
+        weight_gradients = [np.zeros(w.shape) for w in self.weights]
+
+        for sample, label in batch:
+            # Calculate the direction of gradient for a single sample
+            bias_deltas, weight_deltas = self.back_propogation(sample, label)
+
+            # Combine the result with results from previous samples
+            bias_gradients = [b_gradient + b_delta for b_gradient, b_delta in zip(bias_gradients, bias_deltas)]
+            weight_gradients = [w_gradient + w_delta for w_gradient, w_delta in zip(weight_gradients, weight_deltas)]
+
+        """
+        Apply the found gradient to the weights and biases using
+        learning_rate and the batch size
+        """
+
+        self.biases = [b - (learning_rate / len(batch)) * b_gradient for b, b_gradient in zip(self.biases, bias_gradients)]
+        self.weights = [w - (learning_rate / len(batch)) * w_gradient for w, w_gradient in zip(self.weights, weight_gradients)]
+
+    def back_propogation(self, sample, label):
+
+        """
+        Feed a sample through the network and calculate the changes in
+        weights and biases by propagating back through the network in such a
+        way that the cost is minimized
+        """
+
+        bias_deltas = [np.zeros(b.shape) for b in  self.biases]
+        weight_deltas = [np.zeros(w.shape) for w in self.weights]
+
+        """
+        Calculate the activtions for all neurons by feeding a sample through
+        the network
+        """
+
+        self.feed_forward(sample)
+
+        # Theory by 3Blue1Brown: https://youtu.be/tIeHLnjs5U8
+        L = -1
+        
+        """
+        'partial_deltas' is two (of three) parts of the 'chain rule'
+        - the change in cost given a change in a(L) (= cost_function'(a(L), y))
+        - the change in a(L) given a change in z(L) (= sigmoid'(z(L)))
+        """
+
+        partial_deltas = self.cost_function_derivative(self.activations[L], label) * \
+                         self.activation_function_derivative(self.activations[L])
+
+        """
+        Now multiply with the third part of the chain rule,
+        this third part is different for weights and biases.
+        For biases: the change in z(L) given a change in b(L) = 1           [5:46 in the video]
+        For weights: the change in z(L) given a change in w(L) = a(L - 1)   [5:11 in the video]
+        """
+
+        bias_deltas[L] = partial_deltas  # * 1 (but this is redundant)
+        weight_deltas[L] = np.dot(partial_deltas, self.activations[L - 1].T)
+
+        """
+        Continue back propagation, stop at second to last layer, we don't
+        want to adjust the input layer
+        """
+
+        while L > -self.num_layers + 1:
+
+            """
+            To update the partial_deltas for a previous activation layer we
+            need to multiply again with a third part
+            for the previous activation: the change in z(L) given a change in
+            a(L - 1) = w(L)         [6:05 in the video]
+            """
+
+            previous_layer_deltas = np.dot(self.weights[L].T, partial_deltas)
+            
+            """
+            Again calculate the two (of three) parts of the 'chain rule' for
+            the biases and weights and apply the third part separate
+            """
+
+            partial_deltas = previous_layer_deltas * self.activation_function_derivative(self.activations[L - 1])
+            bias_deltas[L - 1] = partial_deltas  # * 1 (but this is redundant)
+            weight_deltas[L - 1] = np.dot(partial_deltas, self.activations[L - 2].T)
+
+            L -= 1
+
+            """
+            This loop can be slightly optimized, but it's done this way to
+            stay in line with the theory
+            """
+
+        return bias_deltas, weight_deltas
+
+    """
+    This method uses the biases and weights to come up with a prediction and
+    returns the prediction
+    """
+
     def predict(self, a): 
-        for w,b in zip(self.weights, self.biases): 
-            a = self.activation(np.matmul(w,a) + b) 
+        for w, b in zip(self.weights, self.biases): 
+            a = self.activation_function(np.matmul(w, a) + b) 
         return a 
+
+    def calculate_average_cost(self, samples, labels):
+        predictions = self.feed_forward(samples)
+        average_cost = sum([self.cost_function(p, l) for p, l in zip(predictions, labels)]) / len(samples)
+        print('Average cost: {0}'.format(average_cost))
+
+    def print_accuracy(self, images, labels):
+        predictions = self.predict(images)
+        num_correct = sum([np.argmax(a) == np.argmax(b) for a,b in zip(predictions, labels)])
+        print('{0}/{1} accuracy: {2}%'.format(num_correct, len(images), (num_correct / len(images)) * 100))
           
-    # This static method calculates the activation of x which is used to
-    # determine the prediction
+    """
+    This static method calculates the activation of x which is used to
+    determine the prediction
+    """
+
     @staticmethod 
-    def activation(x): 
-        return 1 / (1 - np.exp(-x))
+    def activation_function(z): 
+        return sigmoid(z)
+
+    @staticmethod
+    def activation_function_derivative(a):
+
+        """
+        'a' is the activation, or in other words: a = sigmoid(z)
+        (it's designed this way so we don't have to remember the 'z' when all we need is 'a' anyway)
+        """
+
+        return sigmoid_prime(a)
+
+    @staticmethod
+    def cost_function(output, y):
+        return sum_of_squares(output, y)
+
+    @staticmethod
+    def cost_function_derivative(output, y):
+        return sum_of_squares_prime(output, y)
+
+def sum_of_squares(output, y):
+    return sum((a - b) ** 2 for a, b in zip(output, y))[0]
+
+def sum_of_squares_prime(output, y):
+    return 2 * (output - y)
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def sigmoid_prime(x):
+
+    """
+    The derivative of the sigmoid function is actually 'sigmoid(x) * (1 - sigmoid(x))'
+    but if we take sigmoid(x) as input, we can just use the following:
+    """
+
+    return x * (1 - x)
--- Predictor/Predictor/Predictor.py
+++ Predictor/Predictor/Predictor.py
@@ -2,15 +2,48 @@
 import NeuralNetwork as nn
 import numpy as np 
 
-# Initialises the layer sizes for the neural network.  First value is inputs,
-# last in outputs and any number in-between are the 'hidden' layers and how
-# many neurons they have
-layer_sizes = (3,5,10) 
-x = np.ones((layer_sizes[0],1)) 
+with np.load('mnist.npz') as data:
+    training_images = data['training_images']
+    training_labels = data['training_labels']
+
+"""
+Initialises the layer sizes for the neural network.  First value is inputs,
+last in outputs and any number in-between are the 'hidden' layers and how
+many neurons they have
+"""
+
+layer_sizes = (784, 16, 16, 10)
+
+"""
+Number of images used for training the A.I.  The rest rest of the images are
+used for testing the performance
+"""
+
+training_set_size = 5000
+
+training_set_images = training_images[:training_set_size]
+training_set_labels = training_labels[:training_set_size]
+
+test_set_images = training_images[training_set_size:]
+test_set_labels = training_labels[training_set_size:]
 
 # Calls the __init__ method and sets 'net' as the network
-net = nn.NeuralNetwork(layer_sizes) 
-# Calculates the prediction the network gives
-prediction = net.predict(x) 
+net = nn.NeuralNetwork(layer_sizes)
 
-print(prediction)
+# Evalualtes the performance of the system without any training sessions
+net.print_accuracy(training_images, training_labels)
+net.calculate_average_cost(test_set_images, test_set_labels)
+
+# First training sessions
+net.train_sgd(training_set_images, training_set_labels, 8, 20, 4.0)
+
+# Evalualtes the performance of the system without any training sessions
+net.print_accuracy(training_images, training_labels)
+net.calculate_average_cost(test_set_images, test_set_labels)
+
+# Second training session
+net.train_sgd(training_set_images, training_set_labels, 8, 20, 2.0)
+
+# Evaluate performance after second training session
+net.print_accuracy(test_set_images, test_set_labels)
+net.calculate_average_cost(test_set_images, test_set_labels)
